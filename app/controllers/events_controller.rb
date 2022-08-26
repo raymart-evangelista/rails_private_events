30f6d696859aeb2d02a3004ac19b227a2e038bd9
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-08-26 15:54:40 -0700

    working users#show route

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,24 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = @creator.build_event(event_params)
+
+    if @event.save
+      redirect_to "users#show"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :event_date, :event_time)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :events, only: [:index]
-  # resources :users
+  resources :events, only: [:index, :new, :create]
+  resources :users, only: [:show]
 
   root "events#index"
 end
