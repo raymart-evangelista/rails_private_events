5d93280d592bd4837269f3cf4cb7a92553fca284
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-08-29 20:09:23 -0700

    fix associations using TOP example:

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   # a creator makes events; event.creator
-  belongs_to :creator, foreign_key: "creator_id", class_name: "User"
+  belongs_to :creator, foreign_key: :creator_id, class_name: "User"
 
   # has many attendees through AttendeeEvent
-  has_many :attendee_events, class_name: "AttendeeEvent"
-  has_many :attendees, through: :attendee_events, foreign_key: "event_id", source: :attendee
+  has_many :attendee_events, foreign_key: :event_id
+  has_many :attendees, through: :attendee_events, source: :attendee
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   # a creator makes events; creator.events
-  has_many :events, foreign_key: "creator_id", class_name: "Event"
+  has_many :events, foreign_key: :creator_id, class_name: "Event"
 
   # has many events attending through AttendeeEvent
-  has_many :attendee_events, class_name: "AttendeeEvent"
-  has_many :attended_events, through: :attendee_events, foreign_key: "attendee_id", source: :event
+  has_many :attendee_events, foreign_key: :attendee_id
+  has_many :attended_events, through: :attendee_events
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
