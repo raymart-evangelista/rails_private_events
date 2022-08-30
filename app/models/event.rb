70559521de79f2b5ca2a2b7eb9c1bdb3c40281e3
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-08-29 19:04:34 -0700

    add Event->AttendeeEvent->Attendee association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
   # a creator makes events; event.creator
   belongs_to :creator, foreign_key: "creator_id", class_name: "User"
+
+  # has many attendees through AttendeeEvent
+  has_many :attendee_events
+  has_many :attendees, through: :attendee_events, foreign_key: "attendee_id", source: :attendee
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   # a creator makes events; creator.events
   has_many :events, foreign_key: "creator_id", class_name: "Event"
 
+  # has many events attending through AttendeeEvent
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
